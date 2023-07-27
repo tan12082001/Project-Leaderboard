218649d15eb39a9d1b504a23a0d095cc245e4e89
Tanmayi Manku <tanmayitanum@gmail.com>
2023-07-27 23:06:42 +0530

    create the api module with mock game API id

---
+++ src/modules/api.js
@@ -0,0 +1,6 @@
+// Mock game API is created and the Game ID is generated.
+const API_SERVER = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+const GAME_ID = 'issVk2NaSb1SYQP3ntQZ';
+const MY_API = `${API_SERVER}games/${GAME_ID}/scores/`;
+
+export default MY_API;
